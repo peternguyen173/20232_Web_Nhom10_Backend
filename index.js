ac79648534221ce8d4a6408bed231c31b86890d4
Dung <dinhtuandungnguyen@gmail.com>
2024-04-14 20:41:48 +0700

    Add new project

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app =express();
+
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const PORT = 8000;
+
+app.use(bodyparser.json());
+const allowedOrigins = ['https://localhost:3000', 'https://localhost:3001']
+app.use(
+cors({
+    origin:function(origin,callback){
+        if(!origin || allowedOrigins.includes(origon)){
+            callback(null,true);
+        }else{
+            callback(new Error('CORS khong cho phep truy cap'));
+        }
+    }
+})
+)
+
+app.use(cookieParser())
+
+app.get('/',(req,res)=>{
+    res.json({message: 'The API is working'});
+});
+
+app.listen(PORT ,()=>{
+    console.log(`Sever dang chay o cong: ${PORT}` )
+})
