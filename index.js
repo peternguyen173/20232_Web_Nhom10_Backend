314bd54fe973937e91335d7fafa8f1f16405f851
hasonduong <lasduong12@gmail.com>
2024-05-08 15:05:25 +0700

    change

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const PORT = 8000;
 
+const authRoutes = require('./Routes/Auth')
+const adminRoutes = require('./Routes/Admin')
+const movieRoutes = require('./Routes/Movie')
+const imageuploadRoutes = require('./Routes/ImageUploadRoutes')
+require('dotenv').config();
+require('./db')
+
+
 
 app.use(bodyparser.json());
 const allowedOrigins = ['https://localhost:3000', 'https://localhost:3001']
@@ -22,6 +30,11 @@ cors({
 )
 
 app.use(cookieParser())
+
+app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/movie', movieRoutes);
+app.use('/image', imageuploadRoutes);
 
 app.get('/',(req,res)=>{
     res.json({message: 'The API is working'});
