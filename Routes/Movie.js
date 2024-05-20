7034c81439995e2c9f948c620a81eef808be66ee
Dung <dinhtuandungnguyen@gmail.com>
2024-05-20 22:11:39 +0700

    them api thanh toan VNPAY

--- Routes/Movie.js
+++ Routes/Movie.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const Movie = require('../Models/MovieSchema')
 const Screen = require('../Models/ScreenSchema')
+const errorHandler = require('../Middlewares/errorMiddleware');
+const authTokenHandler = require('../Middlewares/checkAuthToken');
+const adminTokenHandler = require('../Middlewares/checkAdminToken');
 
 function createResponse(ok, message, data) {
     return {
@@ -344,6 +347,105 @@ router.get('/test',async(req,res) => {
     })
 })
 
+// Thanh toan VNPAY
+
+router.post('/create_payment_url', function (req, res, next) {
+    var ipAddr = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    var config = require('config');
+    var dateFormat = require('dateformat');
+
+    
+    var tmnCode = config.get('DPKGLXA7');
+    var secretKey = config.get('Y2AW1HT7LQ61ZZG93KHAUDODE7LAWSU1');
+    var vnpUrl = config.get('https://sandbox.vnpayment.vn/paymentv2/vpcpay.html');
+    var returnUrl = config.get('vnp_ReturnUrl');
+
+    var date = new Date();
+
+    var createDate = dateFormat(date, 'yyyymmddHHmmss');
+    var orderId = dateFormat(date, 'HHmmss');
+    var amount = req.body.amount;
+    var bankCode = req.body.bankCode;
+    
+    var orderInfo = req.body.orderDescription;
+    var orderType = req.body.orderType;
+    var locale = req.body.language;
+    if(locale === null || locale === ''){
+        locale = 'vn';
+    }
+    var currCode = 'VND';
+    var vnp_Params = {};
+    vnp_Params['vnp_Version'] = '2.1.0';
+    vnp_Params['vnp_Command'] = 'pay';
+    vnp_Params['vnp_TmnCode'] = tmnCode;
+    // vnp_Params['vnp_Merchant'] = ''
+    vnp_Params['vnp_Locale'] = locale;
+    vnp_Params['vnp_CurrCode'] = currCode;
+    vnp_Params['vnp_TxnRef'] = orderId;
+    vnp_Params['vnp_OrderInfo'] = orderInfo;
+    vnp_Params['vnp_OrderType'] = orderType;
+    vnp_Params['vnp_Amount'] = amount * 100;
+    vnp_Params['vnp_ReturnUrl'] = returnUrl;
+    vnp_Params['vnp_IpAddr'] = ipAddr;
+    vnp_Params['vnp_CreateDate'] = createDate;
+    if(bankCode !== null && bankCode !== ''){
+        vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    vnp_Params = sortObject(vnp_Params);
+
+    var querystring = require('qs');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+    vnp_Params['vnp_SecureHash'] = signed;
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+    res.redirect(vnpUrl)
+});
+
+ 
+
+// Vui lòng tham khảo thêm tại code demo
+
+ 
+router.get('/vnpay_return', function (req, res, next) {
+    var vnp_Params = req.query;
+
+    var secureHash = vnp_Params['vnp_SecureHash'];
+
+    delete vnp_Params['vnp_SecureHash'];
+    delete vnp_Params['vnp_SecureHashType'];
+
+    vnp_Params = sortObject(vnp_Params);
+
+    var config = require('config');
+    var tmnCode = config.get('vnp_TmnCode');
+    var secretKey = config.get('vnp_HashSecret');
+
+    var querystring = require('qs');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+
+    if(secureHash === signed){
+        //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+
+        res.render('success', {code: vnp_Params['vnp_ResponseCode']})
+    } else{
+        res.render('success', {code: '97'})
+    }
+});
+
+
+
+
 router.use(errorHandler)
 
 module.exports = router;
@@ -351,6 +453,3 @@ module.exports = router;
 
 
 
-const errorHandler = require('../Middlewares/errorMiddleware');
-const authTokenHandler = require('../Middlewares/checkAuthToken');
-const adminTokenHandler = require('../Middlewares/checkAdminToken');
