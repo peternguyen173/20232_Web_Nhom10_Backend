1b3d413891ec414d995f35c52e5617e3cf2aed48
Dung <dinhtuandungnguyen@gmail.com>
2024-05-21 13:54:47 +0700

    update demo

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 const express = require('express');
-const app =express();
+const app = express();
 
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const PORT = 8000;
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./Routes/Auth')
 const adminRoutes = require('./Routes/Admin')
 const movieRoutes = require('./Routes/Movie')
-const imageuploadRoutes = require('./Routes/ImageUploadRoutes')
+const imageuploadRoutes = require('./Routes/imageUploadRoutes');
+
 require('dotenv').config();
 require('./db')
 
 
 
-app.use(bodyparser.json());
-const allowedOrigins = ['https://localhost:3000', 'https://localhost:3001']
+app.use(bodyParser.json());
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', "https://sgpcinema-admin.vercel.app", "https://sgpcinema.vercel.app", "https://sgpcinema-peters-projects-5fccdbc6.vercel.app"]; // Add more origins as needed
+// Configure CORS with credentials
 app.use(
-cors({
-    origin:function(origin,callback){
-        if(!origin || allowedOrigins.includes(origon)){
-            callback(null,true);
-        }else{
-            callback(new Error('CORS khong cho phep truy cap'));
-        }
-    }
-})
-)
-
-app.use(cookieParser())
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true, // Allow credentials
+    })
+);
+app.use(cookieParser());
 
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/movie', movieRoutes);
 app.use('/image', imageuploadRoutes);
 
-app.get('/',(req,res)=>{
-    res.json({message: 'The API is working'});
+
+app.get('/', (req, res) => {
+    res.json({ message: 'The API is working' });
 });
 
-app.listen(PORT ,()=>{
-    console.log(`Sever dang chay o cong: ${PORT}` )
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
